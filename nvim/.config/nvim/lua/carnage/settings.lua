ffb6ad6abcefc65844ea7edc57c0000f893d4b63
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2022-04-15 14:24:09 +0200

    nvim: update settings

--- nvim/.config/nvim/lua/carnage/settings.lua
+++ nvim/.config/nvim/lua/carnage/settings.lua
@@ -1,14 +1,12 @@
 vim.o.backup = false -- creates a backup file
 vim.o.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-vim.o.cmdheight = 2 -- more space in the neovim command line for displaying messages
-vim.o.colorcolumn = "99999" -- fixes indentline for now
+vim.o.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.o.completeopt = "menuone,noselect"
 vim.o.conceallevel = 0 -- so that `` is visible in markdown files
 vim.o.fileencoding = "utf-8" -- the encoding written to a file
 vim.o.foldmethod = "manual" -- folding, set to "expr" for treesitter based folding
 vim.o.foldexpr = "" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
 vim.o.guifont = "monospace:h17" -- the font used in graphical neovim applications
-vim.o.hidden = true -- required to keep multiple buffers and open multiple buffers
 vim.o.hlsearch = true -- highlight all matches on previous search pattern
 vim.o.ignorecase = true -- ignore case in search patterns
 vim.o.mouse = "r" -- allow the mouse to be used in neovim
@@ -24,7 +22,7 @@ vim.o.termguicolors = true -- set term gui colors (most terminals support this)
 vim.o.timeoutlen = 500 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.o.title = true -- set the title of window to the value of the titlestring
 vim.o.titlestring = "%<%F%=%l/%L - nvim"
-vim.o.undofile = true -- enable persiskent undo
+vim.o.undofile = true -- enable persistent undo
 vim.o.updatetime = 300 -- faster completion
 vim.o.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
 vim.o.expandtab = true -- convert tabs to spaces
@@ -41,3 +39,9 @@ vim.o.spelllang = "en"
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
 vim.o.laststatus = 3 -- Single status bar
+vim.opt.formatoptions = vim.opt.formatoptions -- see tj's config for more info
+  - "o"
+  + "r"
+  + "c"
+  - "t"
+
