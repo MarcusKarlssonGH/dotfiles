c18eee125aa55c10e41368f82e85751fc4b8d587
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2022-04-16 12:21:05 +0200

    nvim: move lsp keymaps

--- nvim/.config/nvim/lua/carnage/keymappings.lua
+++ nvim/.config/nvim/lua/carnage/keymappings.lua
@@ -53,3 +53,18 @@ vim.api.nvim_set_keymap('i', ',', ',<c-g>u', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '.', '.<c-g>u', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '!', '!<c-g>u', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '?', '?<c-g>u', { noremap = true, silent = true })
+
+-- Lsp
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+vim.api.nvim_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+vim.api.nvim_set_keymap("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+vim.api.nvim_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>Telescope lsp_code_actions<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>dp", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>dn", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>dl", "<cmd>Telescope diagnostics<CR>", opts)
+vim.api.nvim_set_keymap("n", "gl", '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
--- nvim/.config/nvim/lua/carnage/lsp/servers.lua
+++ nvim/.config/nvim/lua/carnage/lsp/servers.lua
@@ -3,26 +3,11 @@ if not status_ok then
   return
 end
 
-local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>Telescope lsp_code_actions<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dp", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dn", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dl", "<cmd>Telescope diagnostics<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-end
-
-local function default_on_attach(client, bufnr)
-  -- TODO: Do we need word highlight?
-  lsp_keymaps(bufnr)
-end
+-- local function default_on_attach(client, bufnr)
+--   -- TODO: Do we need word highlight?
+--   -- TODO: Do we need this?
+--   -- lsp_keymaps(bufnr)
+-- end
 
 local servers = {
   pyright = true,
@@ -49,7 +34,7 @@ local setup_server = function(server, config)
 
   config = vim.tbl_deep_extend("force", {
     -- on_init = custom_init,
-    on_attach = default_on_attach,
+    -- on_attach = default_on_attach,
     -- capabilities = updated_capabilities,
     flags = {
       debounce_text_changes = 150,
