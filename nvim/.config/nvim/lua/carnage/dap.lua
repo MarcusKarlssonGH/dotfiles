cf14b4e31a916fe3108c13b87bc9b087f9e1abdc
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2024-01-15 22:33:52 +0100

    nvim: format dap.lua

--- nvim/.config/nvim/lua/carnage/dap.lua
+++ nvim/.config/nvim/lua/carnage/dap.lua
@@ -1,9 +1,9 @@
 local map = function(lhs, rhs, desc)
-  if desc then
-    desc = "[DAP] " .. desc
-  end
+	if desc then
+		desc = "[DAP] " .. desc
+	end
 
-  vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
+	vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
 end
 map("<F1>", require("dap").step_back, "step_back")
 map("<F2>", require("dap").step_into, "step_into")
@@ -15,78 +15,78 @@ map("<F9>", require("dap").toggle_breakpoint, "toggle_breakpoint")
 
 local dap = require("dap")
 dap.adapters.cppdbg = {
-  id = "cppdbg",
-  type = "executable",
-  command = "/home/marcus/.vscode/extensions/ms-vscode.cpptools-1.15.4-linux-x64/debugAdapters/bin/OpenDebugAD7"
+	id = "cppdbg",
+	type = "executable",
+	command = "/home/marcus/.vscode/extensions/ms-vscode.cpptools-1.15.4-linux-x64/debugAdapters/bin/OpenDebugAD7",
 }
 dap.configurations.cpp = {
-  {
-    name = "Launch file",
-    type = "cppdbg",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopAtEntry = true,
-  },
-  {
-    name = 'Attach to gdbserver :1234',
-    type = 'cppdbg',
-    request = 'launch',
-    MIMode = 'gdb',
-    -- miDebuggerServerAddress = 'localhost:1234',
-    miDebuggerPath = '/usr/bin/gdb',
-    cwd = '${workspaceFolder}',
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-  },
-  {
-    name = 'hello!',
-    type = 'cppdbg',
-    request = 'launch',
-    MIMode = 'gdb',
-    -- miDebuggerServerAddress = 'localhost:1234',
-    miDebuggerPath = '/usr/bin/gdb',
-    cwd = '${workspaceFolder}',
-    program = "/home/marcus/code/rndm/cpp-playground/hello",
-  },
+	{
+		name = "Launch file",
+		type = "cppdbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtEntry = true,
+	},
+	{
+		name = "Attach to gdbserver :1234",
+		type = "cppdbg",
+		request = "launch",
+		MIMode = "gdb",
+		-- miDebuggerServerAddress = 'localhost:1234',
+		miDebuggerPath = "/usr/bin/gdb",
+		cwd = "${workspaceFolder}",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+	},
+	{
+		name = "hello!",
+		type = "cppdbg",
+		request = "launch",
+		MIMode = "gdb",
+		-- miDebuggerServerAddress = 'localhost:1234',
+		miDebuggerPath = "/usr/bin/gdb",
+		cwd = "${workspaceFolder}",
+		program = "/home/marcus/code/rndm/cpp-playground/hello",
+	},
 }
 
 -- Python
 dap.adapters.python = {
-  type = 'executable';
-  command = '/home/marcus/code/rndm/python-playground/debugpy/bin/python';
-  args = { '-m', 'debugpy.adapter' };
+	type = "executable",
+	command = "/home/marcus/code/rndm/python-playground/debugpy/bin/python",
+	args = { "-m", "debugpy.adapter" },
 }
 
 dap.configurations.python = {
-  {
-    type = 'python';
-    request = 'launch';
-    name = "Launch file";
+	{
+		type = "python",
+		request = "launch",
+		name = "Launch file",
 
-    program = "${file}";
-    pythonPath = function()
-      return "/usr/bin/python3"
-      -- local cwd = vim.fn.getcwd()
-      -- if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-      --   return cwd .. '/venv/bin/python'
-    end
-  },
+		program = "${file}",
+		pythonPath = function()
+			local cwd = vim.fn.getcwd()
+			if vim.fn.executable(cwd .. "/.aoc-venv/bin/python") == 1 then
+				return cwd .. "/.aoc-venv/bin/python"
+			else
+				return "/usr/bin/python3"
+			end
+		end,
+	},
 }
 
 local dapui = require("dapui")
 dapui.setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+	dapui.close()
 end
-
-
