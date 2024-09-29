ca9a469e0bce19056b242b27bcb42afe1ad51da2
Marcus Karlsson <marcus.n.o.karlsson@gmail.com>
2024-09-29 15:58:27 +0200

    nvim: disable depricated jumping keymaps

--- nvim/.config/nvim/lua/carnage/keymappings.lua
+++ nvim/.config/nvim/lua/carnage/keymappings.lua
@@ -56,8 +56,9 @@ vim.keymap.set("n", "<leader>cn", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<leader>cp", "<cmd>cprev<CR>zz")
 
 -- Diagnostic
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- local myopts = {count=1}
+-- vim.keymap.set('n', '<leader>nd', vim.diagnostic.jump(myopts), { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', 'asd', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
